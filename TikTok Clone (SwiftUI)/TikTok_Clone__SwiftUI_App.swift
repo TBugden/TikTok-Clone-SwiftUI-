6f92c7f8c7020b0f96ca3e414d678fa1ad6cb27d
Trevor <bugdent@gmail.com>
2022-11-16 11:55:33 -0800

    Initial Commit

---
+++ TikTok Clone (SwiftUI)/TikTok_Clone__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_Clone__SwiftUI_App.swift
+//  TikTok Clone (SwiftUI)
+//
+//  Created by Jesse Chan on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_Clone__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
